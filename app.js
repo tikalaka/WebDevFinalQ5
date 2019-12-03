0414d4bd716a068debb92d257b43f0aba8865ded
tikalaka <baydadan001@gmail.com>
2019-12-03 13:00:08 -0700

    started working on it

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("Hello")
+const express = require("express");
+const session = require("express-session");
+
+let port = 3000;
+
+const app = express();
+
+app.use(session({
+    secret: 'stinky',
+    cookie: {}
+}))
+
+
+app.set("view engine", "pug");
+app.use(express.static(__dirname + "/public"));
+
+app.use(express.urlencoded({extended: true}));
+
+var routes = require('./routes/routes');
+app.use("/",routes);
+
+app.listen(port, function(){
+    console.log("Listening...on..." + port);
+});
